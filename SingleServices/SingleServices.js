55483e010c3fc82f1d966abcd7f4fd71a9b1dd70
Mohamed Mahmoud <125528675+Mohamedmahmmoud@users.noreply.github.com>
2025-07-07 20:56:57 +0300

    Update SingleServices.js

--- SingleServices/SingleServices.js
+++ SingleServices/SingleServices.js
@@ -8,13 +8,13 @@ function initSingleServices() {
 }
 
 function renderSingleServices() {
-  fetch("../SingleServices/SingleServices.html")
+  fetch("SingleServices/SingleServices.html")
     .then((response) => response.text())
     .then((html) => {
       root.innerHTML = html;
 
       const script = document.createElement("script");
-      script.src = "../SingleServices/SingleServices.js";
+      script.src = "SingleServices/SingleServices.js";
       script.onload = () => {
         initAbout();
       };
