0b26357b53aa35fefd1175c6ecf8f9bafe3a286c
Mohamed Mahmoud <125528675+Mohamedmahmmoud@users.noreply.github.com>
2025-07-07 20:55:13 +0300

    Update Home.js

--- Home/Home.js
+++ Home/Home.js
@@ -8,13 +8,13 @@ function initHome() {
 }
 
 function renderHome() {
-  fetch("../Home/Home.html")
+  fetch("Home/Home.html")
     .then((response) => response.text())
     .then((html) => {
       root.innerHTML = html;
 
       const script = document.createElement("script");
-      script.src = "../Home/Home.js";
+      script.src = "Home/Home.js";
       script.onload = () => {
         initAbout();
       };
