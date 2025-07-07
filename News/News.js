0d2c4bad5abb1083e9d1cb3abd108b0bd906219f
Mohamed Mahmoud <125528675+Mohamedmahmmoud@users.noreply.github.com>
2025-07-07 21:03:15 +0300

    Update News.js

--- News/News.js
+++ News/News.js
@@ -8,13 +8,13 @@ function initNews() {
 }
 
 function renderNews() {
-  fetch("News/News.html")
+  fetch("../News/News.html")
     .then((response) => response.text())
     .then((html) => {
       root.innerHTML = html;
 
       const script = document.createElement("script");
-      script.src = "News/News.js";
+      script.src = "../News/News.js";
       script.onload = () => {
         initAbout();
       };
